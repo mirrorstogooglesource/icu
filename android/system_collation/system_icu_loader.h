79a2bcc9141f058747427ad0db240d22867e619f
David 'Digit' Turner <digit@google.com>
2017-06-02 17:11:01 +0200

    [android] Implement icu_enable_system_collation
    This patch is used to enable a custom build configuration
    to reduce the size of the ICU library and data file for
    Chrome on Android, by using the system version of the
    library for collation tables and services.
    
    For details, please read android/system_collation/README
    
    + Remove obsolete scripts/copy_data_android.sh script,
      simply use 'copy_data.sh android' instead.
    
    + Update ICU version number in README.chromium
      (from 58.1 to 59.1)
    
    DO NOT SUBMIT: Need a new patch file under patches/
                   corresponding to the local modifications
                   in source/ directory.
    
    DO NOT SUBMIT: Need proper unit-tests.
    
    Bug:
    Change-Id: I4e11c45fba7456f90b2a5c7970265590931c843a

---
+++ android/system_collation/system_icu_loader.h
@@ -0,0 +1,38 @@
+#ifndef USYSTEM_ICU_LOADER_H
+#define USYSTEM_ICU_LOADER_H
+
+namespace icu {
+
+using FunctionPointer = void(*)(void);
+
+// An abstract interface to load the system version of the ICU library, then
+// find ICUC entry points in it. The implementation is subtle due to the way
+// the library was configured and installed on the platform.
+//
+// Usage is simple:
+//
+//  1) Call SystemIcuLoader::CreateInstance() to create a new instance.
+//
+//  2) Call GetIcuEntryPoint() repeatedly on it to find the address of
+//     ICU C-based API functions based on their unversioned name.
+//
+class SystemIcuLoader {
+public:
+  virtual ~SystemIcuLoader() = default;
+
+  // Return the address of the entry point for the ICU4C function |name|,
+  // which should not use any platform or versioning suffix (i.e. should be
+  // 'ucol_open' instead of 'ucol_open_59' or 'ucol_open_cr'. Return the
+  // entry point's address if any, of nullptr if not found.
+  virtual FunctionPointer GetIcuEntryPoint(const char* name) const = 0;
+
+  // Create a new instance. This tries to locate the system ICU library,
+  // and the corresponding version suffix that is applied to its entry points,
+  // then return a pointer to the corresponding instance on success.
+  // will be nullptr if the library cannot be located.
+  static SystemIcuLoader* CreateInstance();
+};
+
+}  // namespace icu
+
+#endif
