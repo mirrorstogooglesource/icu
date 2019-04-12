db61d464927a3d4af86240b74ce8779d98281acd
Frank Tang <ftang@chromium.org>
2019-04-12 14:26:50 -0700

    update to 64-2-rc
    Bug: 952305
    Change-Id: I32616f805c0d824b80786b502d96a13c201e15b3

--- source/i18n/dtitvfmt.cpp
+++ source/i18n/dtitvfmt.cpp
@@ -83,8 +83,8 @@ UOBJECT_DEFINE_RTTI_IMPLEMENTATION(DateIntervalFormat)
 //        Needed because these data members are modified by const methods of DateIntervalFormat.
 
 static UMutex *gFormatterMutex() {
-    static UMutex *m = new UMutex();
-    return m;
+    static UMutex m = U_MUTEX_INITIALIZER;
+    return &m;
 }
 
 DateIntervalFormat* U_EXPORT2
@@ -1130,7 +1130,9 @@ DateIntervalFormat::setSeparateDateTimePtn(
         }
         setIntervalPattern(UCAL_YEAR, skeleton, bestSkeleton, differenceInfo,
                            &extendedSkeleton, &extendedBestSkeleton);
-    } else {
+        setIntervalPattern(UCAL_ERA, skeleton, bestSkeleton, differenceInfo,
+                           &extendedSkeleton, &extendedBestSkeleton);
+     } else {
         setIntervalPattern(UCAL_MINUTE, skeleton, bestSkeleton, differenceInfo);
         setIntervalPattern(UCAL_HOUR, skeleton, bestSkeleton, differenceInfo);
         setIntervalPattern(UCAL_AM_PM, skeleton, bestSkeleton, differenceInfo);
