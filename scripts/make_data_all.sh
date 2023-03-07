7984e407697f77cb7bb946fa30fc665fb9d4d2d3
schectman <schectman@google.com>
2023-03-07 16:08:40 +0000

    Build flutter icudtl.dat with English collation
    Bug: b/271575658
    Change-Id: Idbd63ee566d0ca80ab7c405dd8c68a772a159ea9

--- scripts/make_data_all.sh
+++ scripts/make_data_all.sh
@@ -8,7 +8,7 @@ function config_data {
   if [ $# -lt 1 ];
   then
     echo "config target missing." >&2
-    echo "Should be (android|cast|chromeos|common|flutter|ios)" >&2
+    echo "Should be (android|cast|chromeos|common|flutter|flutter_desktop|ios)" >&2
     exit 1
   fi
 
@@ -24,7 +24,7 @@ echo "Build the necessary tools"
     Linux/gcc  --disable-tests --disable-layoutex --enable-rpath \
     --prefix="$(pwd)"
 make -j 120
-
+<<SKIP
 echo "Build the filtered data for common"
 (cd data && make clean)
 config_data common
@@ -60,6 +60,12 @@ echo "Build the filtered data for Flutter"
 config_data flutter
 $ICUROOT/flutter/patch_brkitr.sh && make -j 120
 $ICUROOT/scripts/copy_data.sh flutter
+SKIP
+echo "Build the filtered data for Flutter Desktop"
+(cd data && make clean)
+config_data flutter_desktop
+make -j 120
+$ICUROOT/scripts/copy_data.sh flutter_desktop
 
 echo "Clean up the git"
 $ICUROOT/scripts/clean_up_data_source.sh
